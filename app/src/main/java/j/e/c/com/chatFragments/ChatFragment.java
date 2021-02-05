d61d99c45fdd4e70b98ea21ae9c435617d14e7a4
ahsan-malik <malikahsan2001@gmail.com>
2021-02-05 21:38:14 +0500

    #5 2/5 ahsan chat bottom sheet icons added

--- app/src/main/java/j/e/c/com/chatFragments/ChatFragment.java
+++ app/src/main/java/j/e/c/com/chatFragments/ChatFragment.java
@@ -1,13 +1,11 @@
 package j.e.c.com.chatFragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
